0b00514cc4995f1e77346b36d47dd1fdb15ba01e
Chett Tiller <chett.tiller@gmail.com>
2026-08-24 12:55:49 -0400

    through context

--- day-01/game_phone_project/game_phone_app/views.py
+++ day-01/game_phone_project/game_phone_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import datetime
 
 #       views always need a `request` param
 def home(request):
@@ -7,15 +8,12 @@ def home(request):
 
     # template is the HTML your sending
 
+def about(request):
+    context = { 
+        "most_popular_game": "Flappy Bird",
+        "second_popular_game": "Subway Surfer",
+        "third_popular_game": "Pokemon Go",
+        "current_time": datetime.datetime.now()
+    }
 
-# RETURN AT 12:05 EST
-# RETURN AT 12:05 EST
-# RETURN AT 12:05 EST
-# RETURN AT 12:05 EST
-# RETURN AT 12:05 EST
-# RETURN AT 12:05 EST
-# RETURN AT 12:05 EST
-# RETURN AT 12:05 EST
-# RETURN AT 12:05 EST
-# RETURN AT 12:05 EST
-# RETURN AT 12:05 EST
+    return render(request, "game_phone_app/about.html", context)
