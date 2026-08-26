cceb73761a7bc34ffacd9e6d39fda9262319dcfd
Chett Tiller <chett.tiller@gmail.com>
2026-08-26 15:41:08 -0400

    teacher domain

--- day-03/video_rental_project/app/admin.py
+++ day-03/video_rental_project/app/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 # pull the VideoTape model from models.py
-from .models import VideoTape
+from .models import VideoTape, RentalStore, Customer
+from .models import School, Teacher, Student
 
 # register VideoTape so we can see it in the admin panel
 admin.site.register(VideoTape)
+admin.site.register(RentalStore)
+admin.site.register(Customer)
+
+admin.site.register(School)
+admin.site.register(Teacher)
+admin.site.register(Student)
--- day-03/video_rental_project/app/models.py
+++ day-03/video_rental_project/app/models.py
@@ -1,5 +1,26 @@
 from django.db import models
 
+
+# a video tape BELONGS TO a rental store
+# a rental store HAS MANY video tapes
+class RentalStore(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"RentalStore(id={self.id}, name={self.name})"
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    rental_stores = models.ManyToManyField(RentalStore, blank=True)
+
+    def __str__(self):
+        return f"Customer(id={self.id}, name={self.name})"
+
+
 # class - object oriented programming
 # instance - an item in the database
 class VideoTape(models.Model):
@@ -15,6 +36,8 @@ class VideoTape(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     # auto_now means it will auto update the date whenever the row changes
     updated_at = models.DateTimeField(auto_now=True)
+    # associates the video tape to a rental store - a video tape belongs to a store and a store has many tapes
+    rental_store = models.ForeignKey(RentalStore, on_delete=models.CASCADE, related_name="video_tapes", null=True, blank=True)
 
     def __str__(self):
         return f"VideoTape(id={self.id}, title={self.title})"
@@ -38,3 +61,25 @@ class VideoTape(models.Model):
 # # DELETE DATA
 # aladdin = VideoTape.objects.get(title="Aladdin") # get item
 # aladdin.delete() # delete the row from the db
+
+
+class School(models.Model):
+    name = models.CharField(max_length=200)
+    website = models.URLField(max_length=300)
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=200, null=True, blank=True)
+    # teacher belongs to one school
+    # school has many teachers
+    school = models.ForeignKey(School, on_delete=models.CASCADE, related_name="teachers")
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    # students can have a blank gpa bc of blank=True/null=True
+    gpa = models.DecimalField(max_digits=2, decimal_places=1, blank=True, null=True)
+    # teachers have many students
+    # students have many teachers
+    teachers = models.ManyToManyField(Teacher, blank=True, related_name="students")
