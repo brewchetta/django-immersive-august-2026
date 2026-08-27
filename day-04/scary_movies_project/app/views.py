497c5bda2167b09d1316eadfc2ad697ddb0357cb
brewchetta <chett.tiller@gmail.com>
2026-08-27 11:26:26 -0400

    index and detail page set up

--- day-04/scary_movies_project/app/views.py
+++ day-04/scary_movies_project/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Movie
 
 def home(request):
@@ -7,3 +7,10 @@ def home(request):
     # add all_movies to context
     context = { "all_movies": all_movies }
     return render(request, "app/home.html", context)
+
+def movie_detail(request, pk):
+    # movie = Movie.objects.get(pk=pk)
+    movie = get_object_or_404( Movie, pk=pk )
+
+    context = { "movie": movie }
+    return render(request, 'app/movie_detail.html', context)
