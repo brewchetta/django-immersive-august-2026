94f1b207893cae70a7a2c53c4cee53818a8686d9
brewchetta <chett.tiller@gmail.com>
2026-08-28 12:18:58 -0400

    through login

--- day-05/user_project/message_app/forms.py
+++ day-05/user_project/message_app/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from django import forms
 
 # imports the user model for us using a special, safe function
 User = get_user_model()
@@ -10,3 +11,9 @@ class SignUpForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
                             # the passwords need to match to create a user
+
+# a generic form for the login form
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=100)
+    # transform into password input
+    password = forms.CharField(widget=forms.PasswordInput)
--- day-05/user_project/message_app/views.py
+++ day-05/user_project/message_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .forms import SignUpForm
-from django.contrib.auth import login
+from .forms import SignUpForm, LoginForm
+from django.contrib.auth import login, logout, authenticate
 
 def home(request):
     return render(request, 'message_app/home.html')
@@ -22,8 +22,29 @@ def auth_signup(request):
     context = { "form": form }
     return render(request, 'message_app/signup.html', context)
 
-def auth_login():
-    pass
 
-def auth_logout():
-    pass
+def auth_login(request):
+    if (request.method == "POST"):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            # cleaned_data is the data that's been validated
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            # potentially find a user with that username & password
+            user = authenticate(request, username=username, password=password)
+            if (user):
+                # if we do we log them in
+                login(request, user)
+                return redirect('home')
+        # if invalid form or invalid user rerender form & send a message
+        context = { "form": form, "message": "Invalid username or password" }
+        return render(request, 'message_app/login.html', context)
+
+    form = LoginForm()
+    context = { "form": form }
+    return render(request, 'message_app/login.html', context)
+
+
+def auth_logout(request):
+    logout(request)
+    return redirect('home')
